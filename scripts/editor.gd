fd08b2af5ed751692c5ad9514df000eb7b8d51cd
PizzArt <poopip@dontcom.com>
2020-10-04 17:22:20 +0300

    editor complete yahooooooooo

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -1,11 +1,13 @@
 extends Control
 
-signal path_changed
+signal save_path_changed
+signal load_path_changed
 var current_tile = 0
 var start_placed = false
 var start_pos = Vector2()
 var draw = true
 var level_save_path = ""
+var level_load_path = ""
 var level = preload("res://scenes/Example.tscn")
 var player = preload("res://scenes/Player.tscn")
 onready var buttons = [$"CL/UI/1/2/3/Tiles/BG",
@@ -15,7 +17,7 @@ onready var buttons = [$"CL/UI/1/2/3/Tiles/BG",
 	$"CL/UI/1/2/3/Tiles/Break",
 	$"CL/UI/1/2/3/Tiles/I",
 	$"CL/UI/1/2/3/Tiles/Move"]
-onready var tilemap = $Level
+onready var tilemap = $TileMap
 
 
 func _process(_delta):
@@ -49,16 +51,37 @@ func toggle(button, tile):
 
 func save_level():
 	var packed_scene = PackedScene.new()
-	packed_scene.pack($Level)
-	yield(self, "path_changed")
+	packed_scene.pack(tilemap)
+	yield(self, "save_path_changed")
 	ResourceSaver.save(level_save_path, packed_scene)
+
+
+func load_level():
+	tilemap.queue_free()
+	var packed_scene = load(level_load_path)
+	var map = packed_scene.instance()
+	add_child(map)
+	tilemap = map
 
 
 func test_level():
 	var character = player.instance()
-	add_child(character)
 	var lvl = level.instance()
+	remove_child(tilemap)
+	lvl.add_child(tilemap)
+	lvl.add_child(character)
+	character.position = start_pos * 16
+	character.position.y += 8
+	character.position.x += 8
+	print(character.position, start_pos)
 	add_child(lvl)
+	$CL/UI/Menu.hide()
+	$CL/UI/Finish.show()
+
+
+func finish_test():
+	$"CL/UI/1".show()
+	$CL/UI/Finish.hide()
 
 
 func _on_CheckButton_toggled(button_pressed):
@@ -113,8 +136,7 @@ func _on_Save_pressed():
 
 func _on_Save_Dialog_file_selected(path):
 	level_save_path = path
-	level_save_path = path
-	emit_signal("path_changed")
+	emit_signal("save_path_changed")
 
 
 func _on_Save_Dialog_confirmed():
@@ -127,3 +149,29 @@ func _on_Main_Menu_pressed():
 
 func _on_Quit_confirmed():
 	get_tree().change_scene("res://scenes/Menu.tscn")
+
+
+func _on_Test_pressed():
+	test_level()
+
+
+func _on_Finish_pressed():
+	finish_test()
+
+
+func _on_Load_pressed():
+	$"CL/UI/Menu/Dialogs/Load Dialog".popup()
+
+
+func _on_Load_Dialog_file_selected(path):
+	level_load_path = path
+	emit_signal("load_path_changed")
+
+
+func _on_Load_Dialog_confirmed():
+	print("should pop up")
+	$CL/UI/Menu/Dialogs/Overwrite.popup()
+
+
+func _on_Overwrite_confirmed():
+	load_level()
